c11e51a5e1c51d373f292e695a9e382d7ac4cb1b
arjhaye <arjhayepaulino@gmail.com>
2021-02-01 22:00:14 +0800

    My Portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
